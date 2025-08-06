86609b910d04e77cc6022dfa3411bc748ff291c2
Markus Videfors <markus.videfors@internetstiftelsen.se>
2025-08-06 11:43:52 +0200

    Validate URL upon importing from env var

--- {{cookiecutter.module}}/cmd/{{cookiecutter.module}}/main.go
+++ {{cookiecutter.module}}/cmd/{{cookiecutter.module}}/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"net/url"
 	"os"
 	"os/signal"
 	"syscall"
@@ -76,9 +77,14 @@ func main() {
 	}
 
 	/* If set, environment variables override config file */
-	url, exists := os.LookupEnv("TAPIR_ANALYSE_NATS_URL")
-	if exists {
-		mainConf.Nats.Url = url
+	natsURL, exists := os.LookupEnv("TAPIR_ANALYSE_NATS_URL")
+	if exists && natsURL != "" {
+		parsedURL, err := url.Parse(natsURL)
+		if err != nil {
+			fmt.Printf("Invalid NATS URL format from environment variable '%s': %s, exiting...\n", natsURL, err)
+			os.Exit(-1)
+		}
+		mainConf.Nats.Url = parsedURL.String()
 	}
 
 	application, err := setup.BuildApp(mainConf)
