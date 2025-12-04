c01e545334804ad3a96ed933ed41289d52f45263
Markus Videfors <markus.videfors@internetstiftelsen.se>
2025-12-04 12:49:34 +0100

    Boilerplate Github workflow (#14)
    * add CODEOWNERS to generated project
    
    * Add base pipeline for generated project
    
    * add formatting check to project workflow
    
    * run tests in the generated project's workflow
    
    * use fixed go version set by cookiecutter
    
    * Remove dependecy on format being done for build
    
    * Set go version for the whole project from cookiecutter
    
    * Remove default go version
    
    * Require user to set a specific go version
    
    * update cookiecutter test pipeline to use go 1.25.4
    
    * use same go version throughout the cc test pipeline

--- hooks/pre_gen_project.sh
+++ hooks/pre_gen_project.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+
+# Validate that go_version is provided
+if [ -z "{{cookiecutter.go_version}}" ]; then
+    echo "ERROR: go_version is required. Please provide a Go version (e.g., '1.25.4')."
+    exit 1
+fi
+
 exit 0
